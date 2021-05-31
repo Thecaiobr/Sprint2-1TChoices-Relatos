d7cce60b29f305e91a3a656afa6bc233b98210e5
unknown <caio.massote8@Gmail.com>
2021-05-31 12:22:23 -0300

    Primeira versão

---
+++ 1tchoices.js
@@ -0,0 +1,99 @@
+//Menu
+
+
+//Btn Leia Mais
+
+function leiaMaisCardUm(){
+    let pontosUm=document.getElementById("pontosUm");
+    let maisTextoUm=document.getElementById("maisUm");
+    let btnLeiaMaisUm=document.getElementById("btnLeiaMaisUm");
+
+    if(pontosUm.style.display === "none"){
+        pontosUm.style.display="inline";
+        maisTextoUm.style.display="none";
+        btnLeiaMaisUm.innerHTML="Leia Mais"
+    }else{
+        pontosUm.style.display="none";
+        maisTextoUm.style.display="inline";
+        btnLeiaMaisUm.innerHTML="Leia Menos";
+    }
+}
+function leiaMaisCardDois(){
+    let pontosDois=document.getElementById("pontosDois");
+    let maisTextoDois=document.getElementById("maisDois");
+    let btnLeiaMaisDois=document.getElementById("btnLeiaMaisDois");
+
+    if(pontosDois.style.display === "none"){
+        pontosDois.style.display="inline";
+        maisTextoDois.style.display="none";
+        btnLeiaMaisDois.innerHTML="Leia Mais"
+    }else{
+        pontosDois.style.display="none";
+        maisTextoDois.style.display="inline";
+        btnLeiaMaisDois.innerHTML="Leia Menos"
+    }
+}
+
+function leiaMaisCardTres(){
+    let pontosTres=document.getElementById("pontosTres");
+    let maisTextoTres=document.getElementById("maisTres");
+    let btnLeiaMaisTres=document.getElementById("btnLeiaMaisTres");
+
+    if(pontosTres.style.display === "none"){
+        pontosTres.style.display="inline";
+        maisTextoTres.style.display="none";
+        btnLeiaMaisTres.innerHTML="Leia Mais"
+    }else{
+        pontosTres.style.display="none";
+        maisTextoTres.style.display="inline";
+        btnLeiaMaisTres.innerHTML="Leia Menos"
+    }
+}
+
+function leiaMaisCardQuatro(){
+    let pontosQuatro=document.getElementById("pontosQuatro");
+    let maisTextoQuatro=document.getElementById("maisQuatro");
+    let btnLeiaMaisQuatro=document.getElementById("btnLeiaMaisQuatro");
+
+    if(pontosQuatro.style.display === "none"){
+        pontosQuatro.style.display="inline";
+        maisTextoQuatro.style.display="none";
+        btnLeiaMaisQuatro.innerHTML="Leia Mais"
+    }else{
+        pontosQuatro.style.display="none";
+        maisTextoQuatro.style.display="inline";
+        btnLeiaMaisQuatro.innerHTML="Leia Menos"
+    }
+}
+
+function leiaMaisCardCinco(){
+    let pontosCinco=document.getElementById("pontosCinco");
+    let maisTextoCinco=document.getElementById("maisCinco");
+    let btnLeiaMaisCinco=document.getElementById("btnLeiaMaisCinco");
+
+    if(pontosCinco.style.display === "none"){
+        pontosCinco.style.display="inline";
+        maisTextoCinco.style.display="none";
+        btnLeiaMaisCinco.innerHTML="Leia Mais"
+    }else{
+        pontosCinco.style.display="none";
+        maisTextoCinco.style.display="inline";
+        btnLeiaMaisCinco.innerHTML="Leia Menos"
+    }
+}
+
+function leiaMaisCardSeis(){
+    let pontosSeis=document.getElementById("pontosSeis");
+    let maisTextoSeis=document.getElementById("maisSeis");
+    let btnLeiaMaisSeis=document.getElementById("btnLeiaMaisSeis");
+
+    if(pontosSeis.style.display === "none"){
+        pontosSeis.style.display="inline";
+        maisTextoSeis.style.display="none";
+        btnLeiaMaisSeis.innerHTML="Leia Mais"
+    }else{
+        pontosSeis.style.display="none";
+        maisTextoSeis.style.display="inline";
+        btnLeiaMaisSeis.innerHTML="Leia Menos"
+    }
+}
---
+++ index.php
@@ -0,0 +1,1 @@
+<?php  require_once ("1tchoice.html")  ?>
